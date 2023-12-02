9dffaf1c4dbe98835791e5e8670fdddfe4a61f0c
Luiz Eduardo <luizeduardodevelop@gmail.com>
2023-12-02 00:51:04 -0400

    primeira versao
    criei as entidades e seus metodos, com certeza irei mudar muita coisa ainda

---
+++ src/main/java/com/techflex/gerenciamento/Techflex/TechflexApplication.java
@@ -0,0 +1,13 @@
+package com.techflex.gerenciamento.Techflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techflex/gerenciamento/Techflex/TechflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techflex.gerenciamento.Techflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
